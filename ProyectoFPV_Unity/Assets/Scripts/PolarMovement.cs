448ef7446d0c6e23ca98d480f10350f8ee818a5d
WinterSnakes <cubicmarigold58@gmail.com>
2021-04-20 19:21:28 -0700

    Se hizo una segunda version de PolarMovement donde se unen las funciones para generar una general

--- ProyectoFPV_Unity/Assets/Scripts/PolarMovement.cs
+++ ProyectoFPV_Unity/Assets/Scripts/PolarMovement.cs
@@ -6,6 +6,8 @@ public class PolarMovement : MonoBehaviour
 {
     public Transform center;
     public float speed;
+
+    //Este representa la posicion en valores polares
     private Vector2 pos;
 
 
@@ -20,18 +22,16 @@ public class PolarMovement : MonoBehaviour
         //Aqui verificamos si el usuario esta haciendo algun input (ya sea horizontal o vertical), si es asi entonces
         //se manda a la funcion correspondiente el signo del movimiento (Esto puede ser interpretado como si el jugador se quiere mover
         //hacia enfrente o hacia atras)
-        if (Input.GetAxisRaw("Horizontal") != 0)
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
         {
-            horizontalPolarMovement((int)Mathf.Sign(Input.GetAxis("Horizontal")));
+            GeneralPolarMovement( );
         }
-        if (Input.GetAxisRaw("Vertical") != 0)
-        {
-            verticalPolarMovement((int)Mathf.Sign(Input.GetAxis("Vertical")));
-        }
+       
     }
 
-    //Esta funcion se encarga del movimiento "Horizontal", como este se hace de forma diferente que el movimiento vertical decidi hacerlo en dos funciones.
-    void horizontalPolarMovement(int sign){
+    //Esta funcion se encarga del movimiento en general.
+    void GeneralPolarMovement()
+    {
 
         //Esta parte del codigo se encarga de hacer que la rotacion del objeto concorde con su posicion relativa al centro.
         //Primero se obtiene el vector de direccion que apunte del objeto al centro
@@ -41,15 +41,14 @@ public class PolarMovement : MonoBehaviour
         //Se aplica la rotacion con la funcion  Quaternion.LookRotation
         transform.rotation = Quaternion.LookRotation(targetDirection);
 
-        //pos.x += Time.deltaTime;
-        pos.y += Time.deltaTime * (speed * sign);
+        //Como los valores de la variable "pos" representan el vector en coordenadas polares, para poder moverme "Horizontalmente" alrededor del centro
+        //Tengo que aumentar el valor del angulo, en este caso sera el valor representado por "y" dentro del vector
+        //Ademas para poder moverme "Verticalmente" respecto del centro
+        //Tengo que aumentar el valor del radio, en este caso sera el valor representado por "x" dentro del vector
+        pos.y += Time.deltaTime * (speed)* Input.GetAxis("Horizontal");
+        pos.x += Time.deltaTime * (speed) * Input.GetAxis("Vertical");
+        //Despues de aumentar dicho angulo aplico la funcion de coordenadas polares
         transform.position = (Vector2)center.position + new Vector2(pos.x * Mathf.Sin(pos.y), pos.x * Mathf.Cos(pos.y));
     }
 
-    void verticalPolarMovement(int sign)
-    {
-
-        pos.x += Time.deltaTime * (speed * sign);
-        transform.position = (Vector2)center.position + new Vector2(pos.x * Mathf.Sin(pos.y), pos.x * Mathf.Cos(pos.y));
-    }
 }
