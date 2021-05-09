02ad1fe81606842a45b56af0a15cef9b87eb0351
FranciscoValdes-UABC <81935334+FranciscoValdes-UABC@users.noreply.github.com>
2021-05-09 01:23:39 -0700

    Se agregó la escena de menu de inicio. Se agregarón los canvas de Menu pausa y menu de cuando se gana el juego.

--- ProyectoFPV_Unity/Assets/MenuPrincipal.cs
+++ ProyectoFPV_Unity/Assets/MenuPrincipal.cs
@@ -6,15 +6,37 @@ using UnityEngine.SceneManagement;
 public class MenuPrincipal : MonoBehaviour
 {
     public AudioSource Ambiente;
+    public static bool JuegoPausado = false;
+    public GameObject MenuPausaUI;
 
     void Start()
     {
         Ambiente.Play();
     }
 
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().buildIndex != 0 && Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (JuegoPausado)
+            {
+                Reanudar();
+            }
+            else
+            {
+                Pausa();
+            }
+        }
+    }
+
     public void JugarJuego()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void MenuInicial()
+    {
+        SceneManager.LoadScene(0);
     }
 
     public void SalirJuego()
@@ -26,5 +48,19 @@ public class MenuPrincipal : MonoBehaviour
     public void Volumen(float volumen)
     {
         Ambiente.volume = volumen;
+    }
+
+    public void Reanudar()
+    {
+        MenuPausaUI.SetActive(false);
+        Time.timeScale = 1f;
+        JuegoPausado = false;
+    }
+
+    void Pausa()
+    {
+        MenuPausaUI.SetActive(true);
+        Time.timeScale = 0f;
+        JuegoPausado = true;
     }
 }
